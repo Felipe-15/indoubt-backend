37852c246a00362d2db66604de80f6bf68df9f0b
Felipe Souza <felipesouza036@gmail.com>
2025-03-01 15:16:06 -0300

    feat: Add Auth DTO's

---
+++ src/auth/auth.dto.ts
@@ -0,0 +1,25 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class LoginDTO {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class RegisterDTO {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
