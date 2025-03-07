0f18625fe6e2800c390b1467c67d5996c9d8017c
Felipe Souza <felipesouza036@gmail.com>
2025-03-07 13:03:25 -0300

    refactor (Auth): Change Dto's and controller names

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,21 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
-import { LoginDTO } from './dtos/login.dto';
-import { RegisterDTO } from './dtos/register.dto';
+import { LoginDto } from './dtos/login.dto';
+import { RegisterDto } from './dtos/register.dto';
 
 @Controller()
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('login')
-  login(@Body() loginDTO: LoginDTO) {
-    const { email, password } = loginDTO;
+  login(@Body() loginDto: LoginDto) {
+    const { email, password } = loginDto;
 
     return this.authService.login(email, password);
   }
 
   @Post('register')
-  register(@Body() registerDTO: RegisterDTO) {
-    return this.authService.register(registerDTO);
+  register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
   }
 }
