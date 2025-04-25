bdb368c834a1627fd8d9f7afc6c80a9ddbc95fdc
Felipe Souza <felipesouza036@gmail.com>
2025-04-25 18:04:08 -0300

    feat (QuestionController): Add hype routes and search logic

--- src/modules/question/question.controller.ts
+++ src/modules/question/question.controller.ts
@@ -1,11 +1,15 @@
 import {
+  BadRequestException,
   Body,
+  ConflictException,
   Controller,
   Delete,
   Get,
+  NotFoundException,
   Param,
   Patch,
   Post,
+  Query,
   Req,
   UseGuards,
 } from '@nestjs/common';
@@ -14,25 +18,53 @@ import { AuthGuard } from 'src/guards/auth-guard';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { UpdateQuestionDto } from './dto/update-question.dto';
 import { QuestionService } from './question.service';
+import { AlreadyHypedQuestionError } from '../exceptions/AlreadyHypedQuestion';
+import { NeverHypedQuestionError } from '../exceptions/NeverHypedQuestion';
+import { IVectorialService } from './resource/vector/vectorial-service.interface';
 
 @Controller('question')
 export class QuestionController {
-  constructor(private readonly questionService: QuestionService) {}
+  constructor(
+    private readonly questionService: QuestionService,
+    private readonly vectorialService: IVectorialService,
+  ) {}
 
   @UseGuards(AuthGuard)
   @Post()
-  create(@Body() createQuestionDto: CreateQuestionDto, @Req() req: Request) {
+  async create(
+    @Body() createQuestionDto: CreateQuestionDto,
+    @Req() req: Request,
+  ) {
     const userId = (req.user as { id: string; email: string }).id;
 
-    return this.questionService.create({
+    const createdQuestion = await this.questionService.create({
       ...createQuestionDto,
       ownerId: userId,
     });
+
+    await this.vectorialService.create({
+      question: createdQuestion.content,
+      linkedId: createdQuestion.id,
+    });
+
+    return createdQuestion;
   }
 
   @Get()
   findAll() {
     return this.questionService.findAll();
+  }
+
+  @Get('search')
+  async search(@Query('q') query: string) {
+    const vectorResult = await this.vectorialService.search(query);
+    if (!vectorResult) {
+      throw new NotFoundException('No results found');
+    }
+    const questionIds = vectorResult.map((item) => item.linkedId);
+    const resultQuestions =
+      await this.questionService.findManyByIdsArray(questionIds);
+    return resultQuestions;
   }
 
   @Get(':id')
@@ -52,9 +84,37 @@ export class QuestionController {
   }
 
   @UseGuards(AuthGuard)
-  @Delete(':id')
-  remove(@Param('id') id: string, @Req() req: Request) {
+  @Post(':id/hype')
+  hype(@Param('id') id: string, @Req() req: Request) {
     const userId = (req.user as { id: string; email: string }).id;
-    return this.questionService.remove(id, userId);
+    try {
+      return this.questionService.hype(id, userId);
+    } catch (error) {
+      if (error instanceof AlreadyHypedQuestionError) {
+        throw new ConflictException();
+      }
+      throw new NotFoundException();
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Post(':id/unhype')
+  unhype(@Param('id') questionId: string, @Req() req: Request) {
+    const userId = (req.user as { id: string; email: string }).id;
+    try {
+      return this.questionService.unhype(userId, questionId);
+    } catch (error) {
+      if (error instanceof NeverHypedQuestionError) {
+        throw new BadRequestException();
+      }
+      throw new NotFoundException();
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Delete(':id')
+  remove(@Param('id') questionId: string, @Req() req: Request) {
+    const userId = (req.user as { id: string; email: string }).id;
+    return this.questionService.remove(questionId, userId);
   }
 }
